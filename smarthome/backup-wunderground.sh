54bf56ffd7be0c1da8cc56c95a33ab646c6c033f
lukeSky3434 <lukaspendl@gmx.at>
2021-12-17 10:10:57 +0100

    feature: tmp local files

---
+++ smarthome/backup-wunderground.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CDAY=$(date '+%d%m%y')
+FOLDER=backup_wheater_$CDAY
+BACKUP_DIR=/home/pi/influxdb/backup/
+
+echo "mkdir $FOLDER"
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh mkdir $FOLDER
+
+rm -rf $BACKUP_DIR*
+
+sudo docker exec influx influxd backup -portable -database wheater /backup
+
+sudo chmod 666 $BACKUP_DIR* 
+
+echo "uploading files to dropbox"
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload $BACKUP_DIR* $FOLDER/
+echo "done"
+
